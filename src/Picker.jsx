294be57e04710f159f7fa5328a76d367a8c2e253
toyama satoshi <toyamarinyon@gmail.com>
2016-08-22 20:06:45 +0900

    Update design

--- src/Picker.jsx
+++ src/Picker.jsx
@@ -3,32 +3,116 @@ import {
   GoogleMapLoader,
   GoogleMap,
   Marker,
+  Circle
 } from 'react-google-maps';
 
 const propTypes = {
   lat: PropTypes.number.isRequired,
-  lng: PropTypes.number.isRequired
+  lng: PropTypes.number.isRequired,
+  icon: PropTypes.object,
+  dragIcon: PropTypes.object,
 };
 
-function Picker(props) {
-  const position = { lat: props.lat, lng: props.lng };
-  return (
-    <section style={{ height: '100%' }}>
-      <GoogleMapLoader
-        containerElement={
-          <div style={{ height: '100%' }} />
-        }
-        googleMapElement={
-          <GoogleMap defaultZoom={6} defaultCenter={position} >
-            <Marker position={position} draggable />
-          </GoogleMap>
-        }
+const defaultProps = {
+  icon: {
+    url: './1471880603_map-marker.png',
+    scaledSize: new google.maps.Size(48, 48)
+  },
+  dragIcon: {
+    url: './1471880603_map-marker.png',
+    scaledSize: new google.maps.Size(48, 48)
+  }
+};
+
+class Picker extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: {
+        lat: props.lat,
+        lng: props.lng
+      },
+      icon: props.icon,
+      drag: false
+    };
+    this.handleDragend = this.handleDragend.bind(this);
+    this.handleDragstart = this.handleDragstart.bind(this);
+    this.renderCircle = this.renderCircle.bind(this);
+  }
+
+  handleDragend(event) {
+    const newPosition = {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng()
+    };
+    this.setState({
+      position: newPosition,
+      icon: this.props.icon,
+      drag: false
+    });
+    this.googleMap.panTo(new google.maps.LatLng(newPosition));
+  }
+
+  handleDragstart() {
+    this.setState({
+      icon: this.props.dragIcon,
+      drag: true
+    });
+  }
+
+  renderCircle() {
+    if (this.state.drag) {
+      return '';
+    }
+    const options = {
+      strokeColor: '#74b3b9',
+      strokeOpacity: 1,
+      strokeWeight: 2,
+      fillColor: '#9aede6',
+      fillOpacity: 0.5,
+    };
+    return (
+      <Circle
+        center={this.state.position}
+        radius={Math.sqrt(10) * 100}
+        options={options}
       />
-    </section>
-  );
+    );
+  }
+
+  render() {
+    return (
+      <section style={{ height: '100%' }}>
+        <GoogleMapLoader
+          containerElement={
+            <div style={{ height: '100%' }} />
+          }
+          googleMapElement={
+            <GoogleMap
+              defaultZoom={15}
+              defaultCenter={this.state.position}
+              ref={(ref) => { this.googleMap = ref; }}
+            >
+              {this.renderCircle()}
+              <Marker
+                position={this.state.position}
+                draggable
+                onDragend={this.handleDragend}
+                onDragstart={this.handleDragstart}
+                icon={this.state.icon}
+              />
+            </GoogleMap>
+          }
+        />
+      </section>
+    );
+  }
 }
 
 Picker.propTypes = propTypes;
+
+Picker.defaultProps = defaultProps;
 
 export default Picker;
 
--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,9 @@ import Picker from './Picker';
 import './index.css';
 
 ReactDOM.render(
-  <Picker lat={25.0112183} lng={121.52067570000001} />,
+  <Picker
+    lat={35.009992}
+    lng={135.759609}
+  />,
   document.getElementById('root')
 );
