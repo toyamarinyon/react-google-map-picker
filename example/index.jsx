674fe951b9f910eb20dd3e0a35b259bfef88f844
toyama satoshi <toyamarinyon@gmail.com>
2016-08-26 17:12:06 +0900

    Update example

--- example/index.jsx
+++ example/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Picker from '../lib/Picker';
+import Picker from '../src/Picker';
 import './index.css';
 
 ReactDOM.render(
   <div className="container">
-    <h1>React google map picker</h1>
     <Picker
       lat={35.009992}
       lng={135.759609}
+      onChange={value => console.log(value)}
     />
   </div>,
   document.getElementById('root')
